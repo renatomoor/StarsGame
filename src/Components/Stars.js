d46563520f74907722240648eda0441ce97b16d2
renatomoor <renato.moor@liip.ch>
2018-08-20 12:07:09 +0200

    adding maximun 3 stars in a row

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -2,21 +2,16 @@ import React from 'react'
 import '../Style/Global.css';
 import _ from 'lodash';
 
-
-
-
-
-
 export const Stars = (props) => {
-
     return (
         <div className="col-5">
-
-
+            <div className="row">
             {_.range(props.numberOfStars).map(i =>
-                <i key={i} className="fa fa-star"></i>
+                <div className="col-4">
+                    <i  key={i} className="fa fa-star"></i>
+                </div>
             )}
-
+            </div>
         </div>
     )
-}
+};
